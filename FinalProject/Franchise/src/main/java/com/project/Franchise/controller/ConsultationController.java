965b7b776bfbc0147ea714563aae980c2cb8917c
bby270 <qorqudduf@naver.com>
2025-04-10 11:22:48 +0900

    간편문의

--- FinalProject/Franchise/src/main/java/com/project/Franchise/controller/ConsultationController.java
+++ FinalProject/Franchise/src/main/java/com/project/Franchise/controller/ConsultationController.java
@@ -18,14 +18,21 @@ public class ConsultationController {
     public ConsultationController(ConsultationRepository repository) {
         this.repository = repository;
     }
-
+    //일반문의
     @PostMapping
     public ResponseEntity<ConsultationInquiries> saveInquiry(@RequestBody ConsultationInquiries inquiry) {
         ConsultationInquiries savedInquiry = repository.save(inquiry);
         return ResponseEntity.ok(savedInquiry); // 상태 200 OK 반환
         // 또는 ResponseEntity.status(HttpStatus.CREATED).body(savedInquiry);
     }
+    //간단문의
+    @PostMapping("/simple")
+    public ResponseEntity<ConsultationInquiries> saveSimpleInquiry(@RequestBody ConsultationInquiries inquiry) {
+        ConsultationInquiries saved = repository.save(inquiry);
+        return ResponseEntity.ok(saved);
+    }
 
+    //전체문의 목록 조회
     @GetMapping
     public ResponseEntity<List<ConsultationInquiries>> getAllInquiries() {
         List<ConsultationInquiries> inquiries = repository.findAll();
